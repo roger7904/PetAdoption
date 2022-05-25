b9aa85c78b8a413d7a4e2ca8bdb3271f9ba18161
roger <roger@25sprout.com>
2022-05-25 17:17:08 +0800

    feat(Profile): add sign out feature

--- app/src/main/java/com/roger/petadoption/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/roger/petadoption/ui/main/profile/ProfileFragment.kt
@@ -1,6 +1,7 @@
 package com.roger.petadoption.ui.main.profile
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -9,9 +10,13 @@ import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.roger.petadoption.R
 import com.roger.petadoption.databinding.FragmentProfileBinding
+import com.roger.petadoption.ui.auth.SignInActivity
 import com.roger.petadoption.ui.base.BaseFragment
 import com.roger.petadoption.ui.base.BaseViewModel
+import com.roger.petadoption.ui.base.SimpleDialogFragment
+import com.roger.petadoption.ui.base.ViewEvent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,7 +28,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
     override fun initParam(data: Bundle) {
     }
 
-    override fun getViewModel(): BaseViewModel? = viewModel
+    override fun getViewModel(): BaseViewModel = viewModel
 
     override fun bindFragmentListener(context: Context) {
     }
@@ -31,7 +36,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
     override fun initViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): FragmentProfileBinding = FragmentProfileBinding.inflate(inflater, container, false)
 
     override fun initView(savedInstanceState: Bundle?) {
@@ -40,6 +45,36 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
                 .load(auth.currentUser?.photoUrl)
                 .centerCrop()
                 .into(ivAvatar)
+
+            clSignOut.setOnClickListener {
+                showDialog(
+                    SimpleDialogFragment.newInstance(
+                        getString(R.string.profile_sign_out_dialog_title),
+                        getString(R.string.profile_sign_out_dialog_content),
+                        btnConfirm = getString(R.string.confirm),
+                        btnCancel = getString(R.string.cancel),
+                        clickEvent = object : SimpleDialogFragment.ClickEvent {
+                            override fun onConfirmClick() {
+                                viewModel.signOut()
+                            }
+
+                            override fun onCancelClick() {
+                            }
+                        }
+                    )
+                )
+            }
+        }
+    }
+
+    override fun handleViewEvent(event: ViewEvent) {
+        super.handleViewEvent(event)
+        when (event) {
+            ProfileViewEvent.SignOut -> {
+                val intent = Intent(activity, SignInActivity::class.java)
+                startActivity(intent)
+                requireActivity().finishAffinity()
+            }
         }
     }
 
