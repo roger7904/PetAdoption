82e21d8d42e6d86ebd861eec87bb261762a76cfd
roger <roger@25sprout.com>
2022-05-25 14:38:42 +0800

    feat: add sign in layout

---
+++ app/src/main/java/com/roger/petadoption/ui/auth/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.roger.petadoption.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.roger.petadoption.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
