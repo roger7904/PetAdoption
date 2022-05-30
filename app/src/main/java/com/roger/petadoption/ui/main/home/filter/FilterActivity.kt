6b243c2c387c6930bccc42f30c6c35324fe2f6fd
roger <roger@25sprout.com>
2022-05-30 16:21:39 +0800

    feat(Filter): add filter page layout

---
+++ app/src/main/java/com/roger/petadoption/ui/main/home/filter/FilterActivity.kt
@@ -0,0 +1,12 @@
+package com.roger.petadoption.ui.main.home.filter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.roger.petadoption.R
+
+class FilterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filter)
+    }
+}
