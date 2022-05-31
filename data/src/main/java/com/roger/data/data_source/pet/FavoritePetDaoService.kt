79ea45fd49f1afa655b5f618a3902cdf1e0684bf
roger <roger@25sprout.com>
2022-05-31 15:42:50 +0800

    feat(Room): Add FavoritePetDaoService function

--- data/src/main/java/com/roger/data/data_source/pet/FavoritePetDaoService.kt
+++ data/src/main/java/com/roger/data/data_source/pet/FavoritePetDaoService.kt
@@ -1,8 +1,26 @@
 package com.roger.data.data_source.pet
 
 import androidx.room.*
+import com.roger.data.dto.pet.FavoritePetModel
+import com.roger.data.local_storage.DatabaseManager
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Maybe
 
 @Dao
 interface FavoritePetDaoService {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertFavoritePet(favoritePetModel: FavoritePetModel): Completable
 
+    @Query(
+        "DELETE FROM ${DatabaseManager.TABLE_FAVORITE_PET}" +
+                " WHERE ${FavoritePetModel.COL_USER_ID} = :userId" +
+                " AND ${FavoritePetModel.COL_PET_ID} = :petId"
+    )
+    fun deleteFavoritePet(userId: String, petId: String): Completable
+
+    @Query(
+        "SELECT * FROM ${DatabaseManager.TABLE_FAVORITE_PET}" +
+                " WHERE ${FavoritePetModel.COL_USER_ID} = :userId"
+    )
+    fun getFavoritePetList(userId: String): Maybe<List<FavoritePetModel>>
 }
