b6c8e0f7a80cac4ae90e844f3497e64b978f557b
roger <roger@25sprout.com>
2022-06-15 13:35:34 +0800

    feat(Map): implement hospital map route feature

--- app/src/main/java/com/roger/petadoption/ui/main/hospital/detail/HospitalDetailActivity.kt
+++ app/src/main/java/com/roger/petadoption/ui/main/hospital/detail/HospitalDetailActivity.kt
@@ -1,6 +1,7 @@
 package com.roger.petadoption.ui.main.hospital.detail
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Address
 import android.os.Bundle
@@ -20,8 +21,19 @@ import com.roger.petadoption.ui.base.BaseViewModel
 import com.roger.petadoption.utils.BitmapHelper
 import dagger.hilt.android.AndroidEntryPoint
 import android.location.Geocoder
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.DirectionsApi
+import com.google.maps.DirectionsApiRequest
+import com.google.maps.GeoApiContext
+import com.google.maps.model.*
+import com.roger.petadoption.BuildConfig
+import com.roger.petadoption.utils.toPx
 import java.io.IOException
+import java.lang.Exception
 
 @AndroidEntryPoint
 class HospitalDetailActivity : BaseActivity<ActivityHospitalDetailBinding>(), OnMapReadyCallback {
@@ -30,6 +42,11 @@ class HospitalDetailActivity : BaseActivity<ActivityHospitalDetailBinding>(), On
     private var map: GoogleMap? = null
     private var locationPermissionGranted = false
     private var markers: List<Marker>? = null
+    private var polyline: Polyline? = null
+    private var lastKnownLocation: LatLng = DEFAULT_LOCATION
+    private val fusedLocationProviderClient: FusedLocationProviderClient by lazy {
+        LocationServices.getFusedLocationProviderClient(this)
+    }
     private val markerIcon: BitmapDescriptor by lazy {
         BitmapHelper.vectorToBitmap(
             this,
@@ -56,20 +73,31 @@ class HospitalDetailActivity : BaseActivity<ActivityHospitalDetailBinding>(), On
                 addMarkers(it)
             }
 
+            if (viewModel.hospitalLocationList.value.isNullOrEmpty()){
+                btnSearch.visibility = View.GONE
+            }
+
             btnSearch.setOnClickListener {
                 searchArea()
             }
         }
     }
 
+    @SuppressLint("PotentialBehaviorOverride")
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
         map?.setInfoWindowAdapter(HospitalMarkerInfoWindowAdapter(this@HospitalDetailActivity))
 
+        map?.setOnInfoWindowClickListener { marker ->
+            infoWindowClickEvent(marker)
+        }
+
         getLocationPermission()
 
         updateLocationUISetting()
+
+        getDeviceLocation()
     }
 
     private fun addMarkers(hospitalList: List<HospitalEntity>) {
@@ -111,6 +139,91 @@ class HospitalDetailActivity : BaseActivity<ActivityHospitalDetailBinding>(), On
                 }
             }
         }
+    }
+
+    private fun infoWindowClickEvent(marker: Marker) {
+        polyline?.remove()
+        polyline = null
+        val departureLatLng = lastKnownLocation
+        val destinationLatLng = marker.position
+
+        val path: MutableList<LatLng> = ArrayList()
+
+        val context = GeoApiContext.Builder()
+            .apiKey(BuildConfig.GOOGLE_MAPS_API_KEY)
+            .build()
+
+        val req: DirectionsApiRequest =
+            DirectionsApi.getDirections(
+                context,
+                "${departureLatLng.latitude},${departureLatLng.longitude}",
+                "${destinationLatLng.latitude},${destinationLatLng.longitude}"
+            )
+        try {
+            val res: DirectionsResult = req.await()
+
+            //Loop through legs and steps to get encoded polylines of each step
+            if (!res.routes.isNullOrEmpty()) {
+                val route: DirectionsRoute = res.routes[0]
+                if (route.legs != null) {
+                    for (i in route.legs.indices) {
+                        val leg: DirectionsLeg = route.legs[i]
+                        if (leg.steps != null) {
+                            for (j in leg.steps.indices) {
+                                val step: DirectionsStep = leg.steps[j]
+                                if (!step.steps.isNullOrEmpty()) {
+                                    for (element in step.steps) {
+                                        val step1: DirectionsStep = element
+                                        val points1: EncodedPolyline = step1.polyline
+                                        //Decode polyline and add points to list of route coordinates
+                                        val coords1: List<com.google.maps.model.LatLng> =
+                                            points1.decodePath()
+                                        for (coord1 in coords1) {
+                                            path.add(LatLng(coord1.lat, coord1.lng))
+                                        }
+                                    }
+                                } else {
+                                    val points: EncodedPolyline = step.polyline
+                                    //Decode polyline and add points to list of route coordinates
+                                    val coords: MutableList<com.google.maps.model.LatLng>? =
+                                        points.decodePath()
+                                    if (coords != null) {
+                                        for (coord in coords) {
+                                            path.add(LatLng(coord.lat, coord.lng))
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (ex: Exception) {
+            Toast.makeText(
+                this, getString(R.string.shelter_map_route_exception), Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        //Draw the polyline
+        if (path.size > 0) {
+            val opts = PolylineOptions()
+                .addAll(path)
+                .color(ContextCompat.getColor(this, R.color.colorPrimary))
+                .width(5.toPx(this))
+            polyline = map?.addPolyline(opts)
+        }
+
+        val builder = LatLngBounds.Builder()
+        builder.include(departureLatLng)
+        builder.include(destinationLatLng)
+
+        map?.animateCamera(
+            CameraUpdateFactory.newLatLngBounds(
+                builder.build(),
+                200)
+        )
+
+        marker.hideInfoWindow()
     }
 
     private fun getLocationFromAddress(strAddress: String?): LatLng? {
@@ -185,7 +298,26 @@ class HospitalDetailActivity : BaseActivity<ActivityHospitalDetailBinding>(), On
         )
     }
 
+    private fun getDeviceLocation() {
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        lastKnownLocation = LatLng(task.result.latitude, task.result.longitude)
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Toast.makeText(
+                this, getString(R.string.shelter_map_device_location_exception), Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+
     companion object {
+        private val DEFAULT_LOCATION = LatLng(25.0530895, 121.6047654)
         private const val DEFAULT_ZOOM = 12f
         private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
         const val ARG_HOSPITAL_ID = "HOSPITAL_ID"
